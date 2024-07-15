abd06af8af8518694dd3cac2ae18146f581bafec
Biplab Sarkar <biplabskr24@gmail.com>
2024-07-15 18:30:26 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import Splash from "../src/components/Splash"
 import MainAppContent from "../src/components/MainComponent"
-import React from "react";
-import Header from "./components/Header";
-import Slider from "./components/Slider";
-import slideData from './Data/slideData';
-import About from "./components/About";
-import KeyFeatures from "./components/KeyFeatures";
-import Services from "./components/Services";
-import servicesData from './Data/servicesData';
-import Faq from "./components/Faq";
-import faqData from "./Data/faqData";
-import Footer from "./components/Footer";
+
 
 
 
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Splash.css";
 import AmbiQ from "../assets/AmbiQ Logo_White.png"
+import Accident from "../assets/Accident.png"
 
 
 
@@ -12,8 +13,9 @@ const Splash = ({ cards }) => {
     return (
 
         <div className="splash-screen">
+            <div className="splash-background"></div>
+            <img src={Accident} alt="Logo" className="accident-logo"/>
             <img src={AmbiQ} alt="Logo" className="splash-logo" />
-            
         </div>
     );
 };
